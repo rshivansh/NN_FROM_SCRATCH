651c4457da78376dcf5833818ece1aef21eddc08
Shivansh Rao <rao.shivansh570@gmail.com>
2024-02-04 15:54:43 -0800

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,10 +1,3 @@
-nn_architecture = [
-    {"input_dim": 2, "output_dim": 4, "activation": "relu"},
-    {"input_dim": 4, "output_dim": 6, "activation": "relu"},
-    {"input_dim": 6, "output_dim": 6, "activation": "relu"},
-    {"input_dim": 6, "output_dim": 4, "activation": "relu"},
-    {"input_dim": 4, "output_dim": 1, "activation": "sigmoid"},
-]
 
 nn_architecture = [
     {"input_dim": 2, "output_dim": 4, "activation": "relu"},
